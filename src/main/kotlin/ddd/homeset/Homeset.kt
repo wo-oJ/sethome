4691dd42979260279c5c78bc8a737bf42460cdb4
Ethan1319 <simwj1234@gmail.com>
2025-11-15 12:33:38 +0900

    first commit

---
+++ src/main/kotlin/ddd/homeset/Homeset.kt
@@ -0,0 +1,14 @@
+package ddd.homeset
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Homeset : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
