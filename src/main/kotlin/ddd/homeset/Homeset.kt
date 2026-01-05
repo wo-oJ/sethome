26a534bf833b9599c4affdacac5b1caeb8f38e30
wooJ <simwj1234@gmail.com>
2026-01-05 20:02:01 +0900

    Clean up onEnable and onDisable methods
    Removed commented plugin startup and shutdown logic.

--- src/main/kotlin/ddd/homeset/Homeset.kt
+++ src/main/kotlin/ddd/homeset/Homeset.kt
@@ -5,10 +5,10 @@ import org.bukkit.plugin.java.JavaPlugin
 class Homeset : JavaPlugin() {
 
     override fun onEnable() {
-        // Plugin startup logic
+        
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+        
     }
 }
